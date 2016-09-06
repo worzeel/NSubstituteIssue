b3f084421086bb2d5f22bd8beb0bccefef193682
Antonio Broughton <antoniob@attachesoftware.com>
2016-09-06 20:39:50 +1200

    Code to show issue with Arg.Any usage when checking Received on an interface with a parameter that is within an abstract class

---
+++ Issue.Test/IssueAbstractTests.cs
@@ -0,0 +1,73 @@
+﻿using Issue;
+using NUnit.Framework;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Issue.Test
+{
+    [TestFixture]
+    public class IssueAbstractTests
+    {
+        private IIssueInterface issueInterface;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.issueInterface = Substitute.For<IIssueInterface>();
+        }
+
+        private IssueAbstract SUT()
+        {
+            return Substitute.For<IssueAbstract>(issueInterface);
+        }
+
+        [Test]
+        public void TestSomeMethod()
+        {
+            var sut = SUT();
+
+            sut.MyTestMethod();
+
+            this.issueInterface.Received().SomeMethod();
+        }
+
+        [Test]
+        public void TestSomeMethodWithArgAny()
+        {
+            var sut = SUT();
+
+            Arg.Any<string>();
+
+            sut.MyTestMethod();
+
+            this.issueInterface.Received().SomeMethod();
+        }
+
+        [Test]
+        public void TestSomeMethodWithParam()
+        {
+            var sut = SUT();
+
+            sut.MyTestMethod();
+
+            this.issueInterface.Received().SomeMethodWithParam("test");
+        }
+
+        [Test]
+        public void TestSomeMethodWithParamWithArgAny()
+        {
+            var sut = SUT();
+
+            Arg.Any<string>();
+
+            sut.MyTestMethod();
+
+            // this fails for some reason with "No Received"... only difference is the call to Arg.Any...
+            this.issueInterface.Received().SomeMethodWithParam("test");
+        }
+    }
+}
---
+++ Issue/IssueAbstract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Issue
+{
+    public abstract class IssueAbstract
+    {
+        private IIssueInterface issueInterface;
+
+        public IssueAbstract(IIssueInterface issueInterface)
+        {
+            this.issueInterface = issueInterface;
+        }
+
+        public void MyTestMethod()
+        {
+            this.issueInterface.SomeMethod();
+            this.issueInterface.SomeMethodWithParam("test");
+        }
+    }
+}
